9e491f4d364e50b75757142a3503eb2e48585dd2
Joel <joel.salminen@gmail.com>
2018-05-19 16:05:49 +0300

    Correct data is now fetched from api

--- src/components/HighChartTable.js
+++ src/components/HighChartTable.js
@@ -5,8 +5,10 @@ class HighChartTable extends Component{
 	constructor(props){
     super(props);
     this.state = {
-      token: ""
+      token: "",
+      bicycleUtilization: []
     }
+
     this.tokenInputChangeHandler = this.tokenInputChangeHandler.bind(this);
     this.fetchButtonClickHandler = this.fetchButtonClickHandler.bind(this);
   }
@@ -15,22 +17,28 @@ class HighChartTable extends Component{
 
   }
 
+  // Input field for state: token
   tokenInputChangeHandler(evt){
   	this.setState({token: evt.target.value})
   }
 
+  // button functionality, fetches data from api
   fetchButtonClickHandler(evt){
   	$.ajax({
 
   		headers: {
   			'Authorization': 'Bearer ' + this.state.token
   		},
-  		url: 'https://bubvn4vsm7.execute-api.eu-west-1.amazonaws.com/dev/equipment',
+  		url: 'https://bubvn4vsm7.execute-api.eu-west-1.amazonaws.com/dev/utilization?startDatetime=2018-04-16T00:00:00.000000Z&endDatetime=2018-04-29T00:00:00.000000Z&groupBy=timePart,equipment_id&timePart=hour&metric=utilization&equipment_id=194',
   		method: 'get', 
-  		success: (res) => {console.log(res)},
+  		success: (res) => {
+  			this.setState({bicycleUtilization: res});
+  			console.log(this.state.bicycleUtilization);
+  		},
   		error: (err) => {console.log(err)}
   	});
-  	console.log("hi");
+
+
   }
 
 	render(){
