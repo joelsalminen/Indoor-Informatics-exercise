46ff8aab117a531d5367d2cb4116a0afb6d7b06e
Joel <joel.salminen@gmail.com>
2018-05-19 15:32:07 +0300

    Added HightChartTable constructor and ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 import HighChartTable from './components/HighChartTable'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  ComponentDidMount(){
+
+  }
+
   render() {
     return (
       <div className="App">
