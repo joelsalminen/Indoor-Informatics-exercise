fe7c17fc941d2a3e8c271a16b4334641a34a0a5e
Joel <joel.salminen@gmail.com>
2018-05-22 16:55:47 +0300

    Added class BicycleChart

---
+++ src/components/MainComponents/BicycleChart.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Highcharts from 'highcharts';
+import $ from 'jquery'
+
+class BicycleChart extends Component{
+	render(){
+		return(
+			<div id="bicyclechart"></div>
+		);
+	};
+}
+
+export default BicycleChart;
