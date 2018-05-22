93f3d5cfaf4541c2495e67b5d49c734fc7120368
Joel <joel.salminen@gmail.com>
2018-05-22 15:36:07 +0300

    Cardio chart added

--- src/components/HighChartTable.js
+++ src/components/HighChartTable.js
@@ -44,8 +44,6 @@ class HighChartTable extends Component{
  					}
  				});
 
- 				console.log(categories);
-
  				// base of url for Cardio util fetch
  				let url = 'https://bubvn4vsm7.execute-api.eu-west-1.amazonaws.com/dev/utilization?startDatetime=2018-04-30T00:00:00.000000Z&endDatetime=2018-05-07T00:00:00.000000Z&groupBy=timePart,equipment_id&timePart=week&metric=utilization&category_id=';
 
@@ -63,9 +61,57 @@ class HighChartTable extends Component{
  					headers: {
  						'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5rTTVOVFJHUkVFNE5UVXdOekpGTmpKRlJFTXpORUZCUWtJek9FRTBPVGRHTXpJMU5rUXpNQSJ9.eyJodHRwczovL2luZG9vcmluZm9ybWF0aWNzLmNvbS9jbGFpbXMvZ3JvdXBzIjpbIjUiLCI1XzUiXSwiaHR0cHM6Ly9pbmRvb3JpbmZvcm1hdGljcy5jb20vY2xhaW1zL3JvbGVzIjpbIk9yZ2FuaXphdGlvbkFkbWluIl0sImh0dHBzOi8vaW5kb29yaW5mb3JtYXRpY3MuY29tL2NsYWltcy9wZXJtaXNzaW9ucyI6WyJyZWFkOmVxdWlwbWVudCIsInJlYWQ6cHJlbWlzZSIsInJlYWQ6ZGV2aWNlIiwicmVhZDp1c2VyIiwicmVhZDp1dGlsaXphdGlvbiIsInJlYWQ6d2FpdGluZyIsImVkaXQ6cHJlbWlzZSIsImVkaXQ6ZGV2aWNlIiwiZWRpdDplcXVpcG1lbnQiLCJjcmVhdGU6ZXF1aXBtZW50IiwiZWRpdDp1c2VyIiwiY3JlYXRlOnVzZXIiLCJkZWxldGU6dXNlciIsImRlbGV0ZTplcXVpcG1lbnQiLCJyZWFkOmN1c3RvbWVyIiwiZWRpdDpjdXN0b21lciIsInJlYWQ6bW9kZWwiLCJjcmVhdGU6bW9kZWwiLCJyZWFkOmJyYW5kIiwiY3JlYXRlOmJyYW5kIl0sIm5pY2tuYW1lIjoiZGVtby11c2VyIiwibmFtZSI6ImRlbW9AaW5kb29yaW5mb3JtYXRpY3MuY29tIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyLzI2ZTBmNzkxY2VhZjU3ZTFmMTViZTlmMzI2ODRlNDJkP3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGZGUucG5nIiwidXBkYXRlZF9hdCI6IjIwMTgtMDUtMjJUMTA6NTg6MTcuMTg4WiIsImlzcyI6Imh0dHBzOi8vaW5kb29yaW5mb3JtYXRpY3MuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVhY2E0MDVmMDUwZDJjNGVlMmUwMGZiNCIsImF1ZCI6ImJJWUhuSUp6cWxhRk9BazJncW9jZHl6eFFLOXZ6bU1GIiwiaWF0IjoxNTI2OTg2Njk5LCJleHAiOjE1MjcwMjI2OTksImF0X2hhc2giOiJUa3llV1NOcGJ0NU95OXZMMkRCMnJRIiwibm9uY2UiOiJmTH5qQUp2NHBDcUVvRm1saDRZWjgycW9Fbkp-QlpfRiJ9.WY-CDdxY7d3RAmwLr0wFtJMdceyp5HX919IxETI9W-SvyIlzr8syxREWaIoG6QIQ-bE7nLlpuQpgFlYp4NKLHOe77ApD9AgC7H0fYylqqpgMtIun6TOQGiwvAPRZCx2DSiO9Cj1u1289lbr01jT666UQJIs75g1_H7veMTDRR67sNbOZFvSU-tlGuuLmFoYsuKw9kBezwC780ApkEN8Zl57zsCiCHh1H7NNbDKH9pWWGL4tXF9jb_8rMiZJlzT63ddZHi4Lu_Q-BPTdyVd70vYXnthG7eTtn5WVWK3kgLNkCVAwow9YgHX0PDhid2vTd4HXAaBOOMn6DrD8JbeZgvg'
  					},
- 					success: ((cardio)=>{console.log(cardio)}),
+ 					success: ((cardio)=>{
+ 						console.log(cardio);
+ 						// get util data
+ 						let cardioData = [];
+ 						let ids = [];
+ 						cardio.forEach((util)=> {
+ 							cardioData.push(util.utilization);
+ 							ids.push(util.equipment_id);
+ 						});
+
+ 						new Highcharts.Chart({
+		          colors: ["#f7a35c"],
+		          chart: {
+		              type: 'line',
+		              renderTo: 'cardiochart'
+		          },
+		          title: {
+		        		text: ''
+		    			},
+		          yAxis: {
+		          	title: {
+		          		text: "Cardio-laitteiden käyttöaste-%"
+		          	},
+		          	labels: {
+		          		format: '{value}%'
+		          	}
+		          },
+		          xAxis: {
+          			categories: ids
+          		},
+
+		          series: [
+		          	{
+							    name: 'Polkypyörä (pysty) käyttöaste',
+							    data: cardioData
+							  }, 
+
+		          ]
+		  			});
+
+
+
+
+ 					}),
  					error: ((err)=>{console.log(err)})
  				});
+
+
+
+
+
 
 
  				
@@ -104,7 +150,6 @@ class HighChartTable extends Component{
   			this.state.bicycleUtilization.forEach((item) => {
   				// values are multiplied by 100 to get percentage value
   				bicycleData.push(100* item.utilization);
-
   				dates.push(item.timePart);
   			});
   			
