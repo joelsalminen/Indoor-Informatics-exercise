d0633de1817d8e848d81afa1537c9142dadcad3b
Joel <joel.salminen@gmail.com>
2018-05-22 16:07:31 +0300

    Class CardioChart added

---
+++ src/components/MainComponents/CardioChart.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Highcharts from 'highcharts';
+import $ from 'jquery';
+
+class CardioChart extends Component {
+	render(){
+		return(
+			<p> Hello from CardioChart</p>
+		);
+
+	}
+}
+
+export default CardioChart;
+
+// Joel Salminen - joel.salminen@gmail.com
